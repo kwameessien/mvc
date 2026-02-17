958696ec0ed7343b8e30f7dccf1a0dbb25b73692
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-02-16 19:59:50 -0500

    Added middlewares and callback functions from controller directory

--- starting-code/routes/expense.js
+++ starting-code/routes/expense.js
@@ -4,9 +4,16 @@ const express = require('express');
 const router = express.Router();
 
 // require the middlewares and callback functions from the controller directory
-
+const {
+  create,
+  expenseById,
+  read,
+  update,
+  remove,
+  expenseByDate,
+} = require('../controllers');
 // Create POST route to create an expense
-
+router.post('/expense/create', create);
 // Create GET route to read an expense
 
 // Create PUT route to update an expense
