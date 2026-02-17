4dee7864520fc3718a9fb9f272f17b4a146039b9
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-02-16 20:07:49 -0500

    Created GET read a list of expenses

--- starting-code/routes/expense.js
+++ starting-code/routes/expense.js
@@ -21,5 +21,6 @@ router.put('/expense/:id', expenseById, update);
 // Create DELETE route to remove an expense
 router.delete('/expense/:id', remove);
 // Create GET route to read a list of expenses
+router.get('/expense/list/:expenseDate', expenseByDate, read);
 
 module.exports = router;
